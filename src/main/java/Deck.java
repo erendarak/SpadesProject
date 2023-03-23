3e10517d63605f60be931f3350c3779ea6883c83
Eren Darak <erendarak34@gmail.com>
2023-03-23 03:40:56 +0300

    Added LinkedList, Node classes and changed the creation of the deck.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -3,7 +3,7 @@ public class Deck {
     private final String[] suit; //= {"Spade", "Club", "Heart", "Diamond"};
     private final String[] name; //= {"A", "2", "3", "4","5", "6", "7", "8","9", "10", "J", "Q","K"};;
     private int numberOfCards;  //Number of the cards in the deck at that time. Its normally 52.
-    private Card[] cards;  //Array of all the cards
+    private LinkedList cards;  //Array of all the cards
 
     /**
      * Constructor of the deck. 52 cards will be added to the deck.
---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,20 @@
+public class LinkedList {
+
+    protected Node head;  //First element of the linked list
+    protected Node tail;  //Last element of the linked list
+
+    /**
+     * Constructs the linked list of the deck.
+     */
+    public LinkedList(){
+
+    }
+
+    /**
+     * Adds a card of node to the head of the linked list.
+     * @param newCard
+     */
+    public void insertFirst(Node newCard){
+
+    }
+}
