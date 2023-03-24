4a5391e9553c6eb5971040c0fde155d60496a9ef
Eren Darak <erendarak34@gmail.com>
2023-03-24 04:35:17 +0300

    Added Hand class and new methods to the Deck class.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -3,7 +3,7 @@ public class Deck {
     private final String[] suit; //= {"Spade", "Club", "Heart", "Diamond"};
     private final String[] name; //= {"A", "2", "3", "4","5", "6", "7", "8","9", "10", "J", "Q","K"};;
     private int numberOfCards;  //Number of the cards in the deck at that time. Its normally 52.
-    private LinkedList cards;  //Array of all the cards
+    private LinkedList cards;  //Linked List of all the cards
 
     /**
      * Constructor of the deck. 52 cards will be added to the deck.
@@ -17,6 +17,29 @@ public class Deck {
      * @param deck
      */
     public void shuffle(Deck deck){
+
+    }
+
+    /**
+     * Gets the first card of the deck so that Hand class can add cards to the hands of the players.
+     * @return the card(node)
+     */
+    public Node getFirstCardOfDeck(){
+
+    }
+
+    /**
+     * Gets the number of the cards at the deck.
+     * @return numberOfCards
+     */
+    public int getNumberOfCards() {
+
+    }
+
+    /**
+     * Sorts the hand.
+     */
+    public void sortHand(){
 
     }
 }
---
+++ src/main/java/Hand.java
@@ -0,0 +1,47 @@
+public class Hand {
+
+    private Node cards;  //Cards in the linked list of the hand
+    private LinkedList handDeck;  //Linked list of cards in the hand
+    private int numberOfCardsATM;  //Number of cards at that time
+    private int numberOfCardsN=13;  //Number of cards normally
+
+    /**
+     * Constructs the hand class for specified properties to have 13 cards for everyone.
+     * @param numberOfCardsN 13
+     */
+    public Hand(int numberOfCardsN) {
+
+    }
+
+    /**
+     * Adds card to the hand with the getFirstCardOfDeck method of the given deck.
+     * @param deck given deck
+     */
+    public void addCard(Deck deck){
+
+    }
+
+    /**
+     * Deletes card from the hand
+     * @param no index of linked list
+     */
+    public void deleteCard(int no){
+
+    }
+
+    /**
+     * Gets the number of the cards at that moment
+     * @return number of the cards at that moment
+     */
+    public int getNumberOfCardsATM(){
+
+    }
+
+    /**
+     * Displays the hand of the player.
+     * @param player specified player
+     */
+    public void showHand(Player player){
+
+    }
+}
